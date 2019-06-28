85fcfdbf2d02d3724d43bd7260f61aed29587017
Moses Gathuku <mosesgathuku95@gmail.com>
2019-06-29 00:37:24 +0300

    changed production baseUrl

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,6 @@
 <template >
   <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
       <a class="navbar-brand" href="#">Zege</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -25,7 +25,7 @@
 </template>
 <script>
 export default{
-  
+
 }
 
 </script>
