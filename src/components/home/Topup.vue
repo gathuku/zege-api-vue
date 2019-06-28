4ddb72900be899b5b7f275a0437ba21c26f5c006
Moses Gathuku <mosesgathuku95@gmail.com>
2019-06-28 11:02:53 +0300

    wip

--- src/components/home/Topup.vue
+++ src/components/home/Topup.vue
@@ -6,12 +6,47 @@
                   <div class="card-header bg-primary"> <strong> Dashboard</strong></div>
 
                   <div class="card-body">
+                    <div v-if="response.status == 'success'" class="alert alert-success">
+                      {{response.message}}
+                      {{response.balance}}
+                    </div>
+
+                  <div class="form-group col-md-4">
+                    <label for="">Enter Amount</label>
+                  <input v-model="form.amount" class="form-control" type="text" name="madeTo" value="" required>
+                  </div>
+                  <div class="form-group">
+                  <button @click="postTopup" class="btn btn-primary" name="button">Submit</button>
+                  </div>
 
 
-                    Top up 
                   </div>
               </div>
           </div>
       </div>
   </div>
 </template>
+<script>
+  export default{
+    data(){
+      return{
+        response:{},
+        form:{
+          amount:'',
+        }
+      }
+    },
+    methods:{
+      postTopup(){
+        console.log(this.form)
+        this.axios.post('transactions',this.form).then(({data})=>{this.response=data})
+      }
+    },
+    beforeCreate(){
+    this.axios.defaults.headers.common = {'Authorization': `Bearer ${localStorage.token}`}
+    },
+    created(){
+
+    }
+  }
+</script>
--- src/components/home/Transfer.vue
+++ src/components/home/Transfer.vue
@@ -6,12 +6,38 @@
                   <div class="card-header bg-primary"> <strong> Dashboard</strong></div>
 
                   <div class="card-body">
-
-
-                    transfer
-                  </div>
+                    <div class="form-group col-md-5">
+                     <label for="">Enter user Email </label>
+                   <input class="form-control" type="text" name="madeTo" value="" required>
+                   </div>
+                   <div class="form-group col-md-5">
+                     <label for="">Enter Amount</label>
+                   <input class="form-control" type="text" name="amount" value="" required>
+                   </div>
+                   <div class="form-group">
+                   <button class="btn btn-primary" type="submit" name="button">Submit</button>
+                   </div>
+               </div>
               </div>
           </div>
       </div>
   </div>
 </template>
+<script>
+  export default{
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+      postTransfer(){
+        this.axios.post('transaction/transfer',this.form)
+      }
+    },
+    beforeCreate(){
+      this.axios.defaults.headers.common = {'Authorization': `Bearer ${localStorage.token}`}
+    },
+
+  }
+</script>
